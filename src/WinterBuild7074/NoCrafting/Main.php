82a22cd968ffe75a9851738d0eeec6262977ebb1
platformer-maker <philipplippi7@gmail.com>
2017-06-29 15:04:46 +0200

    Adding files.

---
+++ src/WinterBuild7074/NoCrafting/Main.php
@@ -0,0 +1,29 @@
+<?php
+namespace WinterBuild7074\NoCrafting;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\Server;
+use pocketmine\Player;
+
+class Main extends PluginBase implements PluginBase {
+	
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->saveDefaultConfig();
+	}
+
+	public function onTouch(PlayerInteractEvent $event) {
+		$block = $event->getBlock();
+		$blocklevel = $block->getLevel()->getName();
+		$blockedcrafting = $this->getConfig()->get("BlockedCrafting");
+		if($block->getId() === 58) {
+			if(in_array($blocklevel, $blockedcrafting)) {
+				$event->setCancelled();
+			}
+		}
+	}
+}
